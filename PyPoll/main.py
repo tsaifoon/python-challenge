492fdee2189eaac212ebac0dad8b84c140a3a2ba
tsaifoon <etsai1987@gmail.com>
2019-02-02 10:39:20 -0500

    Second commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,11 +17,47 @@ with open(csvPath,newline="") as csvfile:
         county.append(row[1])
         candidate.append(row[2])
 
+print(f"Election Results")
+print(f"-------------------------")
+
 #The total number of votes cast
 totalVotes = 0
 for voter in voterID:
     totalVotes += 1
 
-print(f"Election Results")
-print(f"-------------------------")
 print(f"Total Votes: {totalVotes}")
+print(f"-------------------------")
+
+#A complete list of candidates who received votes
+#create a new array for unique candidates
+uniqCand = []
+for people in candidate:
+    #compare long list with not long list
+        if(people not in uniqCand):
+            uniqCand.append(people)
+
+#The percentage of votes each candidate won
+comboList = []
+for unique in uniqCand:
+    count = 0
+    for people in candidate:            
+        if(people == unique):
+            count += 1
+    comboList.append( [unique,(count/totalVotes * 100),count] )
+    print(f"{unique}: {count/totalVotes * 100}% ({count})")        
+
+print(f"{comboList[0][1]}")
+winName = ""
+winner = 0
+for x in comboList:
+    if(int(comboList[x][1]) > winner):
+        winner = comboList[x][1]
+        winName = comboList[x][0]
+
+print(f"-------------------------")
+print(f"Winner: ")
+print(f"-------------------------")
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote.
+
